8c45fef0cf1f45a30d3bcffb085d0ca6ac031d24
Mateusz Koprowicz <koprowicz.mateusz@gmail.com>
2020-12-19 14:57:47 +0100

    adding custom 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,41 @@
 import React from "react"
+import styled from "styled-components"
+// @ts-expect-error
+import ErrorSvg from "../assets/images/error.svg"
 
 const NotFoundPage = () => (
-  <>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
+  <Wrapper>
+    <StyledError />
+    <h1>Nie znaleziono takiej strony!</h1>
+  </Wrapper>
 )
+
+const Wrapper = styled.div`
+  min-height: 82vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 5rem 2rem;
+  gap: 3rem;
+
+  h1 {
+    color: var(--white);
+    font-size: 4rem;
+    text-align: center;
+  }
+
+  @media (min-width: 768px) {
+    gap: 5rem;
+    h1 {
+      font-size: 5rem;
+    }
+  }
+`
+
+const StyledError = styled(ErrorSvg)`
+  width: 100%;
+  height: 50vh;
+`
 
 export default NotFoundPage
