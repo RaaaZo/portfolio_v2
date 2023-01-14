d937e009e5bc034cf56504a2134748a2c8870209
Mateusz Koprowicz <koprowicz.mateusz@gmail.com>
2023-01-14 23:44:43 +0100

    Updates

---
+++ d0c16330-7e826641c998f3370ab2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{WmFZ:function(t,n,o){"use strict";o.d(n,"a",(function(){return a}));var r=o("Lnxd");function a(t){return Object(r.a)({tag:"svg",attr:{viewBox:"0 0 24 24",strokeWidth:"2",stroke:"currentColor",fill:"none",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"desc",attr:{},child:[]},{tag:"path",attr:{stroke:"none",d:"M0 0h24v24H0z",fill:"none"}},{tag:"path",attr:{d:"M9 15v-6l7.745 10.65a9 9 0 1 1 2.255 -1.993"}},{tag:"path",attr:{d:"M15 12v-3"}}]})(t)}}}]);
+//# sourceMappingURL=d0c16330-7e826641c998f3370ab2.js.map
